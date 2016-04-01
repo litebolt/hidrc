7e9516dddae15a887724284f2b6a6c242ceffb93
litebulb <cornellbunn@gmail.com>
2016-03-31 20:05:05 -0400

    updates

--- hidrc.js
+++ hidrc.js
@@ -1,39 +1,52 @@
 const TcpServer = require('multiwii-msp').TcpServer
 const server = new TcpServer(3002, true)
-const dualShock = require('dualshock-controller');
+const dualShock = require('dualshock-controller')
 const controller = dualShock(
   { config : "dualShock3",
     accelerometerSmoothing : true,
     analogStickSmoothing : false })
 
+var stick = {}
+
+console.log('first stick'+JSON.stringify(stick))
+controller.on('square:press', function (data) {
+  stick = data
+  console.log('second stick'+JSON.stringify(stick))
+})
+
+function log () {
+  console.log('third stick'+JSON.stringify(stick))
+}
+
+setTimeout(log, 5000)
+
 controller.on('error', function(data) {
   console.log('error')
 })
 
-controller.on('square:press', function (data) {
-  console.log('square pressed')
-})
-
-controller.on('square:press', function (data) {
-
-})
-
-
 server.on('register', function (key, device) {
 
   device.on('open', function () {
-    device.setRawRc({}, function (error, ident) {
-      console.log('message sent prolly')
-    })
+
   })
 
   device.on('update', function (data) {
-
+    device.setRawRc({}, function (error, ident) {
+      console.log('send message'+ident)
+      console.log('send data'+stick)
+    })
   })
 
   device.on('close', function () {
 
   })
+  
+    
 })
+
+
+
+
+
 
 
